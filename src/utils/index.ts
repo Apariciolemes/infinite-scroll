e927e7667263249f55fa024d75aef552ec91cb57
Aparicio Lemes Neto <aparicio.neto.3@globalhitss.com.br>
2024-11-14 00:10:18 -0300

    feat: add new utils to remove empty vlaues and debounce of functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,30 @@
+export const debounceFn = (fn: Function, delay: number = 500) => {
+  let idTimer: NodeJS.Timeout
+
+  return (...args: Array<unknown>) => {
+    clearTimeout(idTimer)
+    idTimer = setTimeout(() => {
+      fn(args)
+    }, delay)
+  }
+}
+
+export const removeEmptyValues = <T extends Record<string, number | string | boolean | unknown>>(
+  object: T
+) => {
+  return Object.fromEntries(Object.entries(object).filter(([_, value]) => value))
+}
+
 export const createQueryString = <T extends Record<string, number | string | boolean | undefined>>(
   queryParams: T
 ): string | undefined => {
   if (!queryParams) return
   if (!Object.keys(queryParams).length) return
 
+  const queryParamsOnlyWithValues = removeEmptyValues(queryParams)
   const params = new URLSearchParams()
 
-  Object.entries(queryParams).forEach(([key, value]) => {
+  Object.entries(queryParamsOnlyWithValues).forEach(([key, value]) => {
     if (value !== undefined) {
       if (typeof value === 'object') {
         params.append(key, JSON.stringify(value))
