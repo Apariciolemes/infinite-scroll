a92e57e719688c3afb4f4321c88d0b7dc24c5d6e
Aparicio Lemes Neto <aparicio.neto.3@globalhitss.com.br>
2024-11-14 00:08:12 -0300

    refactor: change the composable from the infinite scroll to intersection observer and accept more targets to observe

--- src/composable/useIntersectionObserver.ts
+++ src/composable/useIntersectionObserver.ts
@@ -1,23 +1,24 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 
-export function useInfiniteScroll(callback: () => void, options = {}) {
+export function useIntersectionObserver(callback: () => void, options = {}) {
   const targetRef = ref<HTMLElement | null>(null)
   const observerRef = ref<IntersectionObserver | null>(null)
 
-  onMounted(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry?.isIntersecting) {
-          callback()
-        }
-      },
-      {
-        root: null,
-        rootMargin: '5px',
-        threshold: 0.1,
-        ...options
+  const callbackEntries = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry?.isIntersecting) {
+        callback()
       }
-    )
+    })
+  }
+
+  onMounted(() => {
+    const observer = new IntersectionObserver(callbackEntries, {
+      root: null,
+      rootMargin: '5px',
+      threshold: 0.3,
+      ...options
+    })
 
     if (targetRef.value) {
       observer.observe(targetRef.value)
