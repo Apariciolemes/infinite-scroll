c648992130be6cd0ca2489caea8e0eed6ba678fd
Aparicio Lemes Neto <aparicio.neto.3@globalhitss.com.br>
2024-11-14 00:06:11 -0300

    feat: create the component base-text-field

---
+++ src/components/BaseTextField/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  const emit = defineEmits<{
+    (e: 'input', value: string): void
+  }>()
+
+  const search = defineModel<string>('')
+
+  const onInput = () => {
+    emit('input', search.value ?? '')
+  }
+</script>
+
+<template>
+  <input
+    type="text"
+    class="font-semibold w-[350px] h-10 text-base text-black rounded shadow-lg outline-none px-4"
+    v-model="search"
+    @input="onInput"
+  />
+</template>
