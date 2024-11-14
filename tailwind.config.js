ddf2939bb66a5e43d51453f7b02cb4b7b175a1e9
Aparicio Lemes Neto <aparicio.neto.3@globalhitss.com.br>
2024-11-14 00:04:55 -0300

    chore: configure tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        thunder: {
+          50: '#f4f2f2',
+          100: '#e4dedd',
+          200: '#cbbfbd',
+          300: '#ad9897',
+          400: '#967979',
+          500: '#876b6d',
+          600: '#745a5e',
+          700: '#5e4a4e',
+          800: '#514246',
+          900: '#473c40',
+          950: '#2a2225'
+        }
+      }
+    }
   },
   plugins: []
 }
